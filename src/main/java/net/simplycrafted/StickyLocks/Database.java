3834e493217febe221d9a4b2f7b9c7a11b773043
Brian Ronald <brian@ppcis.org>
2014-11-30 17:24:38 +0000

    Dependencies no longer required

--- src/main/java/net/simplycrafted/StickyLocks/Database.java
+++ src/main/java/net/simplycrafted/StickyLocks/Database.java
@@ -5,10 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.Chest;
-import org.bukkit.block.DoubleChest;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.InventoryHolder;
 
 import java.sql.*;
 import java.util.ArrayList;
